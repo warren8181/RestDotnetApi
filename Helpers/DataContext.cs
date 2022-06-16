b4a8be87278dab12b1b9a90d191d39704e8ca6a4
warren ngankou <ngankou81@gmail.com>
2022-06-16 23:44:49 +0200

    initial commit

--- Helpers/DataContext.cs
+++ Helpers/DataContext.cs
@@ -14,8 +14,9 @@ public class DataContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        // in memory database used for simplicity, change to a real db for production applications
-        options.UseInMemoryDatabase("TestDb");
+        // connect to mysql with connection string from app settings
+        var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
     }
 
     public DbSet<User> Users { get; set; }
